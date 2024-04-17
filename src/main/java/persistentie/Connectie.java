1a1da0192539e23ea301169a25777774bde92eba
Mohamed Alsagher <mohamed.alsagher@student.hogent.be>
2024-04-17 18:39:29 +0200

    nieuwe pagina aangemaakt, connectie online databank voor iedereen.

--- src/main/java/persistentie/Connectie.java
+++ src/main/java/persistentie/Connectie.java
@@ -1,7 +1,7 @@
 package persistentie;
 
 public class Connectie {
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/db_sdp"; 
-    public static final String DATABASE_USERNAME = "root"; 
-    public static final String DATABASE_PASSWORD = "Y4uycskp"; 
+    public static final String JDBC_URL = "jdbc:mysql://vichogent.be:40058/SDP2_2324_DB_G14"; 
+    public static final String DATABASE_USERNAME = "SDP2-2324-G14"; 
+    public static final String DATABASE_PASSWORD = "groep14ww"; 
 }
