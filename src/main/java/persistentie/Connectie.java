d1f9768a4f538450e85ae14b2deb2913537ef23f
Papuna <papuna.noniashvili@student.hogent.be>
2024-04-17 12:15:30 +0200

    gitignore

--- src/main/java/persistentie/Connectie.java
+++ src/main/java/persistentie/Connectie.java
@@ -4,4 +4,4 @@ public class Connectie {
     public static final String JDBC_URL = "jdbc:mysql://localhost:3306/"; // na / naam van u database 
     public static final String DATABASE_USERNAME = "root"; // normaal is het root 
     public static final String DATABASE_PASSWORD = ""; // uw root password
-}
+} //test
