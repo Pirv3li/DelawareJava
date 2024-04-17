de8a026fced25d0a5dea9b69ddd42d7ed7625dd9
Papuna <papuna.noniashvili@student.hogent.be>
2024-04-17 12:10:52 +0200

    connectie

--- src/main/java/persistentie/Connectie.java
+++ src/main/java/persistentie/Connectie.java
@@ -1,7 +1,7 @@
 package persistentie;
 
 public class Connectie {
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/db_sdp";
-    public static final String DATABASE_USERNAME = "root";
-    public static final String DATABASE_PASSWORD = "Y4uycskp";
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/"; // na / naam van u database 
+    public static final String DATABASE_USERNAME = "root"; // normaal is het root 
+    public static final String DATABASE_PASSWORD = ""; // uw root password
 }
